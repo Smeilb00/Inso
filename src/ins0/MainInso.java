5dbe6503e43bb8d807c57e7382f047b60dddb264
javip <javip@192.168.1.100>
2020-01-14 18:21:19 +0100

    mecagoentuputamadrecalbo

--- src/ins0/MainInso.java
+++ src/ins0/MainInso.java
@@ -3,7 +3,7 @@ package ins0;
 import java.awt.EventQueue;
 
 import ins0.Vista.VentanaLogin;
- 
+  
 public class MainInso {
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
